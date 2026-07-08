9293bc8dae21f2c450c358c5e1cb6a2adaed70d1
sanbital <104039495+sanbital@users.noreply.github.com>
2026-07-08 14:05:31 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.MUNIVERSE_CONFIG = {
+  supabaseUrl: "https://demo1234.supabase.co",
+  supabaseAnonKey: "eyJDEMOKEY",
+  shareUrl: "https://myid.github.io/idol-camp/",
+  gisu: 1, maxShown: 300
+};
