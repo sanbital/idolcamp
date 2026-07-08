0e737524f2e069b4c55178dff1378d7f85953156
sanbital <104039495+sanbital@users.noreply.github.com>
2026-07-08 20:46:49 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.MUNIVERSE_CONFIG = {
   supabaseUrl: "https://kkaoerbblpuszptiibvo.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrYW9lcmJibHB1c3pwdGlpYnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NDY5MTMsImV4cCI6MjA5ODEyMjkxM30.Xf549NzokL9zY7AT8Jd5NYFRj81r7z2hS6i7kZbpCMw",
   shareUrl: "https://sanbital.github.io/idolcamp/",
-  gisu: 1, maxShown: 300
+  gisu: 1, maxShown: 1000
 };
