6809365117d50a88b7f06b696a187cba711ee5f8
sanbital <104039495+sanbital@users.noreply.github.com>
2026-08-08 00:09:07 +0900

    Supabase 설정값 원복

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MUNIVERSE_CONFIG = {
   supabaseUrl: "https://kkaoerbblpuszptiibvo.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6ImtrYW9lcmJibHB1c3pwdGlpYnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NDY5MTMsImV4cCI6MjA5ODEyMjkxM30.Xf549NzokL9zY7AT8Jd5NYFRj81r7z2hS6i7kZbpCMw",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrYW9lcmJibHB1c3pwdGlpYnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NDY5MTMsImV4cCI6MjA5ODEyMjkxM30.Xf549NzokL9zY7AT8Jd5NYFRj81r7z2hS6i7kZbpCMw",
   shareUrl: "https://idolcamp.muniverse.io/",
   gisu: 1, maxShown: 1000,
 
